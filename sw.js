e88c561d49f08b84abc61a67550cb8a2a554090f
ohioo0652-crypto <ohioo0652@gmail.com>
2026-09-01 16:45:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='solulu-shell-v1';
+const CACHE='solulu-shell-v2';
 const APP=['./','./index-pub.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
